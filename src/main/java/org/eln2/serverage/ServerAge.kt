8674b9eced5d15810c60209825105aad3f88839e
Jared Dunbar <jrddunbr@ja4.org>
2022-01-08 22:40:00 -0500

    Changes by Gregory Maddra

--- src/main/java/org/eln2/serverage/ServerAge.kt
+++ src/main/java/org/eln2/serverage/ServerAge.kt
@@ -1,17 +1,37 @@
 package org.eln2.serverage
 
+import net.minecraft.client.gui.screens.MenuScreens
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.core.Registry
+import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.inventory.MenuType
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.Block
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.api.distmarker.OnlyIn
+import net.minecraftforge.client.ConfigGuiHandler
 import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.IExtensionPoint
+import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.registries.ForgeRegistries
+import net.minecraftforge.registries.ForgeRegistry
+import net.minecraftforge.registries.ObjectHolder
+import net.minecraftforge.registries.RegistryObject
+import net.minecraftforge.registries.DeferredRegister
 import org.eln2.serverage.blocks.*
 import org.eln2.serverage.items.*
+import software.amazon.awssdk.services.cloudformation.model.RegistryType
 import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.forge.addGenericListener
+import java.io.Serial
+import kotlin.math.E
 
 @Mod(ServerAge.MODID)
 object ServerAge {
@@ -20,6 +40,16 @@ object ServerAge {
     val serverAgeTab = object: CreativeModeTab("Server_Age") {
         override fun makeIcon(): ItemStack {
             return ItemStack(Items.BRICKS)
+        }
+    }
+
+    @JvmStatic
+    private var deferredContainerRegistry = DeferredRegister.create(ForgeRegistries.CONTAINERS, MODID)
+    var serialConsoleMenuType: RegistryObject<MenuType<SerialConsoleMenu>> = deferredContainerRegistry.register(
+        SerialConsole.name
+    ) {
+        MenuType<SerialConsoleMenu> { id, inv ->
+            SerialConsoleMenu(id, inv)
         }
     }
 
@@ -32,10 +62,19 @@ object ServerAge {
 
         MOD_BUS.register(this)
         FORGE_BUS.register(this)
+
+        deferredContainerRegistry.register(MOD_BUS)
     }
 
     val modItems = mutableMapOf<String, Item>()
     val modBlocks = mutableMapOf<String, Block>()
+
+    @SubscribeEvent
+    fun clientSetupEvent(event: FMLClientSetupEvent) {
+        event.enqueueWork {
+            MenuScreens.register(serialConsoleMenuType.get(), ::SerialConsoleScreen)
+        }
+    }
 
     private fun registerItems() {
         MOD_BUS.addGenericListener({ event: RegistryEvent.Register<Item> -> run {
